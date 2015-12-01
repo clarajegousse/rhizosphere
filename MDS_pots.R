601a659cbb6a5bd59f97ab3f568ab0bb415be22d
Clara <clara.jegousse@gmail.com>
2015-12-01 09:03:19 +0100

    Update and rename MDS.R to MDS_pots.R

--- MDS_pots.R
+++ MDS_pots.R
@@ -1,6 +1,7 @@
 # script R
 # 2015.11.25
 # MDS Multidimensional Scaling
+# plant pots are the individuals, and all variables are used
 # save synthese sheet from Data_ecophy_pour_Christophe_def_v2-1.xlsx as synthese.csv
 
 bacteria <- read.table("phylum_abundance.txt", header=TRUE, sep="\t", dec=".")
@@ -30,5 +31,13 @@ x <- fit$points[, 1]
 y <- fit$points[, 2]
 
 # draw plot
+pdf("MDS_pots.pdf")
+plot(x, y, main="MDS solution in 2 dimensions of the plant pots data")
+dev.off()
+
+# plot with the number of the pots as labels
 plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", main="Metric MDS", type="n")
 text(x, y, labels = row.names(data), cex=.7) 
+
+# from the plot, we can see that we have no clusters, or specific shape :
+# which suggests that there was no biaise during the collect of experimental data
