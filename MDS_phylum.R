0f997ebbb18624c8d3eb47a34d0f29f92bfd5666
Clara <clara.jegousse@gmail.com>
2015-12-01 09:48:49 +0100

    Create MDS_phylum.R

---
+++ MDS_phylum.R
@@ -0,0 +1,42 @@
+# script R
+# 2015.12.1
+# MDS Multidimensional Scaling
+# bacteria phylum are considered as the individuals
+
+# data importation
+data <- read.table("../data/phylum_abundance.txt", header=TRUE, sep="\t", dec=".")
+head(data)
+dim(data)
+
+# libraries
+library(cluster) # https://cran.r-project.org/web/packages/cluster/cluster.pdf
+#library(reshape2) # to do the transpose dataframe or matrix
+
+# transpose
+t.data <- t(data[2:32]) # remove data[1] which is the sequencing id
+
+# calculs des dissimilaritées avec daisy
+m <- daisy(t.data)
+m <- as.matrix(m)
+
+# check
+dim(m)
+summary(m)
+
+# MDS avec cmdscale
+mds1 <- cmdscale(m, eig = TRUE, k = 2) # https://stat.ethz.ch/R-manual/R-devel/library/stats/html/cmdscale.html
+x <- mds1$points[, 1]
+y <- mds1$points[, 2]
+
+# draw basic plot
+plot(x, y, xlab = "", ylab = "")
+
+# draw plot with phylum names
+plot(x, y, xlab = "", ylab = "", type="n")
+text(x, y, labels = row.names(m), cex=.7) 
+
+# save plot as pdf
+pdf("MDS_phylum.pdf")
+plot(x, y, type="n", main="MDS solution in 2 dimensions of migroorganisms phylum of the rhizosphere")
+text(x, y, labels = row.names(m), cex=.7) 
+dev.off()
