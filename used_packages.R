445565b9c576b0f288081e9e452c1b216533ca58
Clara <clara.jegousse@gmail.com>
2015-12-17 10:31:51 +0100

    Update used_packages.R

--- used_packages.R
+++ used_packages.R
@@ -2,12 +2,17 @@
 # 2015.12.02
 # to install all required packages
 
-install.packages("cluster", "igraph", "vegan", "network")
+install.packages("cluster")
+install.packages("igraph")
+install.packages("vegan")
+install.packages("network")
+install.packages("DLBCL")
 
 library(network)
 library(cluster)
 library(igraph)
 library(vegan)
+library(DLBCL) # http://rgm.ogalab.net/RGM/R_rdfile?f=BioNet/man/saveNetwork.Rd&d=R_BC
 
 # igraph to convert to leda format :
 # http://www.inside-r.org/packages/cran/igraph/docs/write.graph
