e273f88a8f312897239b2a0b5b703109a208b519
Clara <clara.jegousse@gmail.com>
2016-01-05 09:51:40 +0100

    Update using_igraph.R

--- using_igraph.R
+++ using_igraph.R
@@ -1,12 +1,13 @@
 # http://www.r-bloggers.com/network-visualization-in-r-with-the-igraph-package/
 
-bacteria <- read.table("data/phylum_abundance.txt", header=TRUE, sep="\t", dec=".")
+bacteria <- read.table("phylum_abundance.txt", header=TRUE, sep="\t", dec=".")
 head(bacteria)
 dim(bacteria)
 
+# to remove Unknown and Unclassified bacteria phylum
 bacteria <- bacteria[1:30]
 
-synthese <- read.table("data/synthese.csv", header=TRUE, sep=",", dec=".")
+synthese <- read.table("synthese.csv", header=TRUE, sep=",", dec=".")
 head(synthese)
 dim(synthese)
 
@@ -14,9 +15,12 @@ data <- merge(synthese, bacteria)
 head(data)
 dim(data)
 
-data.network<-graph.data.frame(data, directed=F)
+# transpose (other option is to use the melt function from the reshape2 package)
+t.data <- t(data[2:32]) # remove data[1] which is the sequencing id
 
-#prints the list of vertices (bacteria)
+data.network<-graph.data.frame(t.data, directed=F)
+
+#prints the list of vertices or nodes (bacteria)
 V(data.network)
 
 #prints the list of edges (relationships)
@@ -28,10 +32,10 @@ degree(data.network)
 # plot(data.network) # impossible to read
 
 #identify those vertices part of less than three edges
-bad.vs<-V(data.network)[degree(data.network)<3]
+#bad.vs<-V(data.network)[degree(data.network)<3]
 
 #exclude them from the graph
-data.network<-delete.vertices(data.network, bad.vs) 
+#data.network<-delete.vertices(data.network, bad.vs) 
 
 V(data.network)$color<-ifelse(V(data.network)$Biomasse.totale_VS3 > 0.6, 'green', 'red') 
 
@@ -43,27 +47,32 @@ V(data.network)$size<-degree(data.network)
 plot(	data.network, 
 	layout = layout.fruchterman.reingold, 
 	main = data.network$name,
-	vertex.label = V(data.network)$name,
+	vertex.label = NA, #V(data.network)$label,
 	vertex.size = 2,
 	vertex.color= V(data.network)$color,
 	vertex.frame.color= "white",
-	vertex.label.color = "white",
+	vertex.label.color = "black",
 	vertex.label.family = "sans",
+	vertex.label = NA,
 	edge.width=E(data.network)$weight, 
-	edge.color="black")
+	edge.color="black"
+	)
 
 
+
+
+
+	
 plot(data.network,
 	layout=layout.fruchterman.reingold,
 	main='Organizational network example',	
 	vertex.label = V(data.network)$name)
-
-
 	vertex.label.dist=0.5,			
 	vertex.label.font=1,			
 	#vertex.label=V(data.network)$Biomasse.totale_VS3,		
 	vertex.label.cex=1			
 )
+
 vertex.label = V(G)$name,
 plot(data.network,
 	layout=layout.fruchterman.reingold,
